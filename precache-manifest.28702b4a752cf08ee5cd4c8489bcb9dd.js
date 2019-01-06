b87cab3e9401e60ffd70f76e2719da96f9abffc0
Jentle Yow <lonestaramazed@live.com>
2019-01-06 12:22:13 +0800

    Updates

---
+++ precache-manifest.28702b4a752cf08ee5cd4c8489bcb9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f7f3daf9341a95c3fe",
+    "url": "/jentleyow/contactmanager/static/js/runtime~main.38f7f3da.js"
+  },
+  {
+    "revision": "e9f5850f95f10be87125",
+    "url": "/jentleyow/contactmanager/static/js/main.e9f5850f.chunk.js"
+  },
+  {
+    "revision": "dc085fcd976e769c002a",
+    "url": "/jentleyow/contactmanager/static/js/1.dc085fcd.chunk.js"
+  },
+  {
+    "revision": "e9f5850f95f10be87125",
+    "url": "/jentleyow/contactmanager/static/css/main.c3b33a12.chunk.css"
+  },
+  {
+    "revision": "dc085fcd976e769c002a",
+    "url": "/jentleyow/contactmanager/static/css/1.4124c823.chunk.css"
+  },
+  {
+    "revision": "352cd41e8eb33fd3124ede93fe18940a",
+    "url": "/jentleyow/contactmanager/index.html"
+  }
+];
